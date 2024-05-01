c9a3be457694aa996b78d8f63495fc718d8cca96
ViktorKohler <151533466+ViktorKohler@users.noreply.github.com>
2024-05-01 18:44:43 +0200

    v.18
    Onsdag

--- js/script.js
+++ js/script.js
@@ -8,3 +8,29 @@ function toggleMenu() {
 }
 
 burger.addEventListener('click', toggleMenu)
+
+/*click*/
+document.addEventListener("DOMContentLoaded", function() {
+    const imageUrls = [
+        "img/plikt_bak.jpg", 
+        "img/cpr_train.jpg", 
+        "img/heart_doctor.jpg",
+        "img/helping.jpg",
+    ];
+
+    let currentIndex = 0;
+    const image = document.getElementById("plikt_bak");
+
+    const leftButton = document.getElementById("but_left");
+    const rightButton = document.getElementById("but_right");
+
+    leftButton.addEventListener("click", function() {
+        currentIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+        image.src = imageUrls[currentIndex];
+    });
+
+    rightButton.addEventListener("click", function() {
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+        image.src = imageUrls[currentIndex];
+    });
+});
